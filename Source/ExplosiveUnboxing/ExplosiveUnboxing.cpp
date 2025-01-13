53fb6a535ff6e3de8db48cbc966a7f67cc6ee484
Alfie <alfiejay@mail.com>
2025-01-13 19:15:37 +0000

    Initial commit

---
+++ Source/ExplosiveUnboxing/ExplosiveUnboxing.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ExplosiveUnboxing.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ExplosiveUnboxing, "ExplosiveUnboxing" );
